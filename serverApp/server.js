5d7c2a3834b078c827116c26a85a8d2b8cf168fe
onurkybsi <onurbpm@outlook.com>
2020-08-27 12:35:07 +0300

    Initial commit

--- serverApp/server.js
+++ serverApp/server.js
@@ -1,1 +1,14 @@
-//yap işi bitir işi T H E K R A L
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const router = express.Router();
+
+router.use(bodyParser());
+router.use(express.static(path.join(__dirname, "../client-app/build")));
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client-app/build"));
+});
+
+const app = express().use(router).listen(3000);
