3114d6bf9d85ac4e253b1a68de1bb26ea6408945
Aleyna Güner <aleynaguner@hotmail.com>
2020-10-01 16:14:32 +0300

    Fix navbar-toggler with jquery imp

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 ReactDOM.render(
   <React.StrictMode>
