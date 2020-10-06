0697ded643ea5ea738f83ce1e174697dd0825e87
onurkybsi <onurbpm@outlook.com>
2020-10-07 00:03:05 +0300

    Klasör yapısı düzeltildi

--- client-app/src/layout/Header.js
+++ client-app/src/layout/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./image/gonulluatolyeleri.png";
 import { HeaderLink } from "./HeaderLink";
-import VolunteerButton from "../volunteerButton/VolunteerButton";
+import VolunteerButton from "./VolunteerButton";
 import "./style/header.css";
 
 export default class Header extends Component {
---
+++ client-app/src/layout/VolunteerButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style/volunteerButton.css";
+
+export default class VolunteerButton extends Component {
+  render() {
+    return (
+      <div>
+        <a href="#">
+          <button class="button">GÖNÜLLÜ OL !</button>
+        </a>
+      </div>
+    );
+  }
+}
