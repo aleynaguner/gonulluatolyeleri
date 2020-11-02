7c73df271b8f9762178a18709a79013256db210b
onurkybsi <onurbpm@outlook.com>
2020-11-02 19:46:41 +0300

    models-validator added

--- serverApp/server.js
+++ serverApp/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const modelsValidator = require("models-validator");
 
 const config = require("./config");
 
@@ -11,36 +12,35 @@ const router = express.Router();
 
 router.use(bodyParser());
 router.use(express.static(path.join(__dirname, "../client-app/build")));
+router.use(
+  modelsValidator.modelValidatorMiddleware({
+    "/api/sendEmail": modelsValidator.createModel("sendEmailModel", {
+      topic: "string",
+      message: "string",
+    }),
+  })
+);
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client-app/build"));
 });
 
 router.post("/api/sendEmail", async (req, res) => {
-  let validationResult = requestValidator.validateSendEmail(req.body);
+  let emailServiceResponse = await emailService.sendEmail(
+    {
+      user: config.senderUser,
+      pass: config.senderPass,
+    },
+    config.emailService,
+    config.receiverUser,
+    req.body.topic,
+    req.body.message
+  );
 
-  if (!validationResult)
-    res.status(400).send({
-      requestValidation: false,
-      message: "Send subject and message information correctly!",
-    });
-  else {
-    let emailServiceResponse = await emailService.sendEmail(
-      {
-        user: config.senderUser,
-        pass: config.senderPass,
-      },
-      config.emailService,
-      config.receiverUser,
-      req.body.topic,
-      req.body.message
-    );
-
-    res.status(emailServiceResponse.responseCode).send({
-      isSuccess: emailServiceResponse.isSuccess,
-      message: emailServiceResponse.message,
-    });
-  }
+  res.status(emailServiceResponse.responseCode).send({
+    isSuccess: emailServiceResponse.isSuccess,
+    message: emailServiceResponse.message,
+  });
 });
 
 // Yukarıda belirtilen pointler hariç tüm GET requestleri "/" ya yani React app yönlendirir.
