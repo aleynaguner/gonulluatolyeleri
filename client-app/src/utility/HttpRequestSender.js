2d2968414904f0cd479cadc6809ba35325e51fff
onurkybsi <onurbpm@outlook.com>
2020-12-26 00:09:18 +0300

    SendRequest added with callback

--- client-app/src/utility/HttpRequestSender.js
+++ client-app/src/utility/HttpRequestSender.js
@@ -29,6 +29,38 @@ export class HttpRequestSender {
 
     return response;
   };
+
+  SendRequest = (method, endpoint, data, callback) => {
+    let res = {
+      isSuccess: false,
+      responseData: null,
+    };
+
+    Axios.request({
+      method: method,
+      url: `${this.baseUrl}/${endpoint}`,
+      data: data,
+    })
+      .then(function (response) {
+        res.isSuccess =
+          responseFromWebService.status >= 200 &&
+          responseFromWebService.status < 300;
+        response.responseData = responseFromWebService.data;
+
+        callback(res);
+      })
+      .catch(function (error) {
+        console.log(error);
+
+        response.isSuccess = false;
+        response.responseData = err;
+      })
+      .then(function () {
+        // always executed
+      });
+
+    return res;
+  };
 }
 
 export const SendRequest = async (method, url, data) => {
